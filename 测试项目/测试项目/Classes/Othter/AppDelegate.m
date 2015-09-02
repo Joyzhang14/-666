2c828ba749d868d403ea195663d0ca3b98aef210
q2939606 <mac@yangyue.local>
2015-09-03 03:35:01 +0800

    1

---
+++ 测试项目/测试项目/Classes/Othter/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  测试项目
+//
+//  Created by mac on 15/9/3.
+//  Copyright (c) 2015年 yy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+        
+    return YES;
+}
+
+
+
+@end
